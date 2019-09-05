3798f204e870e2d4a8e2ff312298855bc36d8005
AVK-Chaitanya <vca10@iitbbs.ac.in>
2019-09-05 14:41:57 +0530

    Add files via upload
    Assignment-5

--- featureNormalize.m
+++ featureNormalize.m
@@ -1,15 +1,17 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
 
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = mean(X);
+X_norm = bsxfun(@minus, X, mu);
 
-mu = mean(X,1);
+sigma = std(X_norm);
+X_norm = bsxfun(@rdivide, X_norm, sigma);
 
-sigma = std(X,1);
 
-for i =1:size(X,1)
-    X_norm(i,:) = (X_norm(i,:)-mu)./sigma;
-end
-X = X_norm;
+% ============================================================
+
 end
